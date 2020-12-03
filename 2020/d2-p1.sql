e9a304355b577e846d12a40d6e03cd87adab4979
DanielFGray <DanielFGray@gmail.com>
2020-12-02 20:41:10 -0600

    2020-d2: use lateral

--- 2020/d2-p1.sql
+++ 2020/d2-p1.sql
@@ -3,15 +3,12 @@ create table rules (rule text);
 \copy numbers from stdin
 --\copy numbers from program 'curl -b session.cookie https://adventofcode.com/2020/day/2/input';
 
-select count(*) from (
-  with rules as (
-    select regexp_split_to_array(rule, '[ :-]+') as str
-    from rules
-  )
+with t as (
   select
     str[1]::int as min,
     str[2]::int as max,
     array_length(array(select unnest(regexp_matches(str[4], str[3], 'g'))), 1) as char_length
-  from rules
-) as t
+  from rules as row,
+  lateral regexp_split_to_array(row.rule, '[ :-]+') as str
+) select count(*) from t
   where t.char_length between t.min and t.max;
--- 2020/d2-p2.sql
+++ 2020/d2-p2.sql
@@ -1,14 +1,11 @@
-select count(*) from (
-  with rules as (
-    select regexp_split_to_array(rule, '[ :-]+') as str
-    from rules
-  )
+with t as (
   select
     str[1]::int as a,
     str[2]::int as b,
     str[3] as char,
     regexp_split_to_array(str[4], '') as pattern
-  from rules
-) as t
+  from rules as t,
+  lateral regexp_split_to_array(rule, '[ :-]+') as str
+) select count(*) from t
   where t.pattern[t.a] = t.char and t.pattern[t.b] != t.char
   or    t.pattern[t.a] != t.char and t.pattern[t.b] = t.char;

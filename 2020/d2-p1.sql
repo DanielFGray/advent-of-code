82bd671b9ed3da2bbeea75d3f358914b3db4f506
DanielFGray <DanielFGray@gmail.com>
2020-12-02 20:59:24 -0600

    2020-d2: format and rename vars

--- 2020/d2-p1.sql
+++ 2020/d2-p1.sql
@@ -5,10 +5,14 @@ create table rules (rule text);
 
 with t as (
   select
-    str[1]::int as min,
-    str[2]::int as max,
-    array_length(array(select unnest(regexp_matches(str[4], str[3], 'g'))), 1) as char_length
+    split[1]::int as min,
+    split[2]::int as max,
+    array_length(array(select unnest(regexp_matches(split[4], split[3], 'g'))), 1) as char_length
   from rules as row,
-  lateral regexp_split_to_array(row.rule, '[ :-]+') as str
-) select count(*) from t
-  where t.char_length between t.min and t.max;
+  lateral regexp_split_to_array(row.rule, '[ :-]+') as split
+)
+select
+  count(*)
+  from t
+  where t.char_length
+  between t.min and t.max;
--- 2020/d2-p2.sql
+++ 2020/d2-p2.sql
@@ -1,11 +1,15 @@
 with t as (
   select
-    str[1]::int as a,
-    str[2]::int as b,
-    str[3] as char,
-    regexp_split_to_array(str[4], '') as pattern
+    split[1]::int as a,
+    split[2]::int as b,
+    split[3] as char,
+    regexp_split_to_array(split[4], '') as pattern
   from rules as t,
-  lateral regexp_split_to_array(rule, '[ :-]+') as str
-) select count(*) from t
-  where t.pattern[t.a] = t.char and t.pattern[t.b] != t.char
-  or    t.pattern[t.a] != t.char and t.pattern[t.b] = t.char;
+  lateral regexp_split_to_array(rule, '[ :-]+') as split
+)
+select
+  count(*)
+  from t
+  where t.char
+  in (t.pattern[t.a], t.pattern[t.b])
+  and t.pattern[t.a] <> t.pattern[t.b]

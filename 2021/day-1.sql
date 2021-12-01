af846f2db7c0972507e60528991c46f8e7c5ff9e
DanielFGray <DanielFGray@gmail.com>
2021-12-01 16:58:01 -0600

    simplify 2021d1 solutions

--- 2021/day-1.sql
+++ 2021/day-1.sql
@@ -6,27 +6,17 @@ create table depths (
 -- \copy depths(n) from program 'curl "https://adventofcode.com/2021/day/1/input" -H "Cookie: session=..."'
 
 -- part 1
-with cte as (
-  select n - lag(n) over w as increased
+with diffs as (
+  select n - lag(n) over (order by row) as increase
   from depths
-    window w as (order by row)
 )
-select count(*)
-from cte
-where increased > 0;
+select count(*) from diffs
+where increase > 0;
 
 -- part 2
-with cte as (
-  select
-    row,
-    sum(n) over (order by row rows between 2 preceding and current row)
+with diffs as (
+  select lead(n, 3) over (order by row) - n as increase
   from depths
-  offset 2
-),
-cte2 as (
-  select sum > lag(sum) over (order by row) as increase
-  from cte
 )
-select count(*)
-from cte2
-where increase;
+select count(*) from diffs
+where increase > 0;
